41df87814edd8d2b088c6c929dc6a5e15c612841
Alex Olson <einstein.ako@gmail.com>
2014-02-20 12:21:49 -0700

    more dummy data for #3

--- js/controllers/vehicles.controller.js
+++ js/controllers/vehicles.controller.js
@@ -14,8 +14,32 @@ define(["backbone", "jquery", "underscore", "views/vehicles.view"], function(Bac
                 "mileage": 159774,
                 "lastWaypoint": {
                    "timestamp": "20140116T152440+0000",
-                "latitude": 28.088505,
-                "longitude": -82.578467
+                    "latitude": 28.088505,
+                    "longitude": -82.578467
+                },
+                "timestamp": "20140116T151952+0000",
+                "running": false,
+                "fulerate": 1.2,
+                "fuellevel" : 20,
+                "coolantTemperature": 163,
+                "batteryVoltage": 13.2,
+                "odometer": 65345,
+                "header": 272,
+                "speed": 72
+            },
+            {
+                "vin": "FAKE_VIN",
+                "nickname" : "",
+                "year" : "2000",
+                "make" : "Toyota",
+                "model" : "Tacoma",
+                "icon" : "https://s3.amazonaws.com/k-mycloud/a169x672/B87948E0-2306-11E3-953D-B39BDC00B96D.img?q=49420",
+                "notes" : "I love this truck!",
+                "mileage": 273000,
+                "lastWaypoint": {
+                   "timestamp": "20140116T152440+0000",
+                    "latitude": 28.088505,
+                    "longitude": -82.578467
                 },
                 "timestamp": "20140116T151952+0000",
                 "running": false,
