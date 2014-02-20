ec9b66369254f96bd10f6675210e229585f7c0f5
Alex Olson <einstein.ako@gmail.com>
2014-02-20 12:11:55 -0700

    remove files.

--- js/controllers/vehicles.controller.js
+++ js/controllers/vehicles.controller.js
@@ -24,7 +24,7 @@ define(["backbone", "jquery", "underscore", "views/vehicles.view"], function(Bac
                 "coolantTemperature": 163,
                 "batteryVoltage": 13.2,
                 "odometer": 65345,
-                "header": 272,
+                "header": 272,// foo
                 "speed": 72
             }]);
         }
