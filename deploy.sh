51f1f81049b51e56d3cbddfda9e5bd3b720b6199
Alex Olson <einstein.ako@gmail.com>
2014-02-21 10:24:54 -0700

    deploy should remove stale files.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 # this puts the current fuse code out on https://fuse.kynetx.com/
 # should be run from the root repo directory.
-rsync -av --exclude "docs" --exclude ".git" --exclude "README.md" --exclude "deploy.sh" --exclude "build.js" --exclude "builds" --update --progress --stats . root@webhost.kynetx.com:/var/www/html/fuse_kynetx_com/
+ssh root@webhost.kynetx.com "cd /var/www/html/fuse_kynetx_com/; rm -rf *"
+rsync -av --exclude "docs" --exclude ".git" --exclude "README.md" --exclude "deploy.sh" --exclude "build.js" --exclude "builds" --exclude ".gitignore" --update --progress --stats . root@webhost.kynetx.com:/var/www/html/fuse_kynetx_com/
