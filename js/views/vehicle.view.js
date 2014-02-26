099af9a48de506caeb9fbbe369b8a7c3a258eed2
Alex Olson <einstein.ako@gmail.com>
2014-02-25 17:29:22 -0700

    data role should be page.

--- js/views/vehicle.view.js
+++ js/views/vehicle.view.js
@@ -1,5 +1,8 @@
 define(["fuse", "jquery", "underscore", "models/vehicle.model", "text!templates/vehicledetailtmpl.html"], function(Fuse, $, _, VehicleModel, vehicleDetailTmpl) {
 	return Fuse.View.extend({
 		tagName: "div"
+		attributes: {
+			"data-role": "page"
+		}
 	});
 });
