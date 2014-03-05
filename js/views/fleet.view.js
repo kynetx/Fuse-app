0fe6215c984e5a091197625418ebbc05bc9a6c66
Alex Olson <einstein.ako@gmail.com>
2014-03-05 13:58:08 -0700

    setting up fleet view.

--- js/views/fleet.view.js
+++ js/views/fleet.view.js
@@ -1,7 +1,16 @@
 define(["fuse", "jquery", "underscore"], function(Fuse, $, _) {
 	return Fuse.View.extend({
+		tagName: "div",
+		id: "fleet",
+		role: "page",
+		header: "Fleet",
+		transition: "slide",
+
 		initialize: function() {
 			Fuse.log("Fleet view initialization.");
+			this.mapConfig = {
+				container: "#fleet"
+			};
 		}
 	});
 });
