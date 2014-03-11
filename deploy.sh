73793544d99c9d2a8594f6d04c24453379a28e7a
Alex Olson <einstein.ako@gmail.com>
2014-03-11 12:16:29 -0600

    simplify deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 # this puts the current fuse code out on https://fuse.kynetx.com/
 # should be run from the root repo directory.
 ssh root@webhost.kynetx.com "cd /var/www/html/fuse_kynetx_com/; rm -rf *"
-rsync -av --exclude "docs" --exclude ".git" --exclude "README.md" --exclude "deploy.sh" --exclude "build.js" --exclude "builds" --exclude ".gitignore" --update --progress --stats . root@webhost.kynetx.com:/var/www/html/fuse_kynetx_com/
+echo "Old build removed from fuse.kynetx.com"
+rsync -av --include "dist" --include "index.html" --exclude "*" --update --progress --stats . root@webhost.kynetx.com:/var/www/html/fuse_kynetx_com/
+echo "New build deployed to fuse.kynetx.com"
