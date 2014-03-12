83cff4720fadcd54355f8c5149805894afd99b32
Alex Olson <einstein.ako@gmail.com>
2014-03-11 21:17:48 -0600

    more deploy script sauce.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,20 @@
 # this puts the current fuse code out on https://fuse.kynetx.com/
 # should be run from the root repo directory.
-ssh root@webhost.kynetx.com "cd /var/www/html/fuse_kynetx_com/; rm -rf .*; rm -rf *"
+
+# if the dist directory doesnt exist, just die as it doesnt make sense
+# do depoy when theres nothing there to deploy
+if [ ! -d "dist" ]; then
+	echo "No dist directory! Cannot deploy fuse. Aborting."
+	exit 1
+fi
+
+# remove stale build
+echo "Removing old build from fuse.kynetx.com"
+ssh root@webhost.kynetx.com "cd /var/www/html/fuse_kynetx_com/; rm -rf *"
 echo "Old build removed from fuse.kynetx.com"
+
+# put current build out on production
+echo "Deploying new build to fuse.kynetx.com"
 rsync -av --exclude "js" --exclude "build.js" \
 --exclude ".git" --exclude ".gitignore" --exclude ".DS_Store" --exclude "deploy.sh" \
 --exclude "README.md" --exclude "docs" --exclude "style/*.css" \
