083ce4f19bdc0cefa56f2ebcfb807c30f693b2ec
Alex Olson <einstein.ako@gmail.com>
2014-03-11 22:23:59 -0600

    dont actually remove unminified source files in builds. We'll probably want them around so we can look at and understand different versions of Fuse.

--- build.sh
+++ build.sh
@@ -6,10 +6,4 @@ echo "Javascript built."
 echo "Buidling CSS..."
 r.js -o style/build.js
 echo "CSS Built."
-echo "Removing stale source files..."
-rm -rf js
-rm -rf docs
-rm -rf style/*.js
-rm -rf style/*.css
-echo "Stale source files removed."
 echo "Fuse is almost ready to be deployed. Just update index.html to include the minified JS & CSS files and then run deploy.sh. Yay!"
