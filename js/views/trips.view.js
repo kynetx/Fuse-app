bf804698c7cca38f9ebf7d2e6abfb8f71dc292ff
Alex Olson <alexkellenolson@gmail.com>
2014-03-19 12:00:17 -0600

    pushing code for trips tool.

--- js/views/trips.view.js
+++ js/views/trips.view.js
@@ -1,5 +1,15 @@
 define([ "backbone", "fuse", "jquery", "underscore" ], function( Backbone, Fuse, $, _ ) {
     // trips view.
     return Fuse.View.extend({
+        id: "trips",
+        tagName: "div",
+        role: "page",
+        header: "Trips",
+        transiton: "slide",
+        tripsTemplate: _.template( tripsTmpl ),
+
+        initialize: function() {
+            Fuse.View.prototype.initialize.apply( this, arguments );
+        }
     });
 });
