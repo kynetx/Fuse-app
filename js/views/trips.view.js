24aec01349b91028ac370efef8ce060408cb5456
Alex Olson <alexkellenolson@gmail.com>
2014-03-19 18:19:26 -0600

    last work for the day. Tomorrow will be awesome.

--- js/views/trips.view.js
+++ js/views/trips.view.js
@@ -1,10 +1,9 @@
-define([ "backbone", "fuse", "jquery", "underscore", "text!templates/tripstmpl.html" ], function( Backbone, Fuse, $, _, tripsTmpl ) {
+define([ "backbone", "fuse", "jquery", "underscore", "collections/trip.collection" "text!templates/tripstmpl.html" ], function( Backbone, Fuse, $, _, TripCollection, tripsTmpl ) {
     // trips view.
     return Fuse.View.extend({
         id: "trips",
         tagName: "div",
         role: "page",
-        header: "Trips",
         transiton: "slide",
         tripsTemplate: _.template( tripsTmpl ),
 
@@ -16,20 +15,22 @@ define([ "backbone", "fuse", "jquery", "underscore", "text!templates/tripstmpl.h
         render: function() {
             this.trips.length = 0;
 
-            this.collection.each(function( trip ) {
-                this.renderTrip( trip );
+            this.collection.each(function( vehicle ) {
+                this.composeTripData( vehicle );
             });
 
             this.content = this.tripsTemplate({ trips: this.trips });
             Fuse.View.prototype.render.call( this );
         },
 
-        renderTrip: function( trip ) {
+        composeTripData: function( vehicle ) {
+            // To be continued...
+            _.each( vehicle.get( "trips" ), this.composeTripList);
             var tripView = new TripView({
                 model: trip
             });
 
-            this.trips.push( tripView.render().el );
+            this.trips.push({ vehicle: vehicle.get( "nickname" ), trips: trips });
         }
     });
 });
