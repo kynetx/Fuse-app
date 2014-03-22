b6c6a31fb0a9381399cd1cda11a805e39f0052a5
Alex Olson <alexkellenolson@gmail.com>
2014-03-22 14:23:33 -0600

    fleet aggregates.

--- js/main.js
+++ js/main.js
@@ -8,83 +8,90 @@ require(["fuse", "cloudos", "jquery", "routers/app.router", "routers/fleet.route
 
 	// setup dummy data. Will eventually come from API obviously.
 	Fuse.FIXTURES = {
-		"fleet": [{
-			"id": "VSPQ",
-			"default": 1,
-			"vin": "1FTFW1EV6AKA75407",
-			"nickname" : "F-150",
-			"year" : "2010",
-			"make" : "Ford",
-			"model" : "F-150 Supercrew",
-			"icon" : "https://s3.amazonaws.com/k-mycloud/a169x672/7BD0B300-7DDF-11E2-AB3A-B9D7E71C24E1.img?q=97013",
-			"notes" : "",
-			"mileage": 159774,
-			"lastWaypoint": {
-				"timestamp": "20140116T152440+0000",
-				"latitude": 37.473896,
-				"longitude": -122.23114
-			},
-			"timestamp": "20140116T151952+0000",
-			"running": false,
-			"fuelRate": 1.2,
-			"fuelLevel" : 37,
-			"coolantTemperature": 163,
-			"batteryVoltage": 13.2,
-			"odometer": 65345,
-			"header": 272,
-			"speed": 0
-		},
-		{
-			"id": "V4GM",
-			"vin": "4TAWNSFDDSKLFJ",
-			"nickname" : "Tacoma",
-			"year" : "2000",
-			"make" : "Toyota",
-			"model" : "Tacoma",
-			"icon" : "https://s3.amazonaws.com/k-mycloud/a169x672/B87948E0-2306-11E3-953D-B39BDC00B96D.img?q=49420",
-			"notes" : "I love this truck!",
-			"mileage": 273000,
-			"lastWaypoint": {
-				"timestamp": "20140116T152440+0000",
-				"latitude": 40.42970,
-				"longitude": -111.89830
-			},
-			"timestamp": "20140116T151952+0000",
-			"running": true,
-			"fuelRate": 1.2,
-			"fuelLevel" : 89,
-			"coolantTemperature": 163,
-			"batteryVoltage": 11.1,
-			"odometer": 65345,
-			"header": 272,
-			"speed": 78
-		},
-		{
-			"id": "VN0E",
-			"vin": "Y257JX9AXQDXI529",
-			"nickname": "Fiesta",
-			"year": "1998",
-			"make": "Ford",
-			"model": "Fiesta",
-			"icon": "style/images/ford_fiesta.jpg",
-			"notes": "Labore consectetur et proident veniam mollit. In minim reprehenderit proident et. Elit fugiat cillum sit cillum quis. Elit ad ullamco velit laborum amet exercitation ea. Nisi ea nostrud ullamco ipsum magna consequat id qui",
-			"mileage": 84519,
-			"lastWaypoint": {
-				"timestamp": "19925606T185601T+07:00",
-				"latitude": 40.74937,
-				"longitude": -111.82633
-			},
-			"timestamp": "19910831T060849T+07:00",
-			"running": true,
-			"fuelRate": 2.63,
-			"fuelLevel": 22,
-			"coolantTemperature": 95,
-			"batteryVoltage": 12.4,
-			"odometer": 19725,
-			"heading": 270,
-			"speed": 12
-		}],
+		"fleet": {
+			"aggregates": {
+				"trip": {
 
+				}
+			},
+			"index": [{
+				"id": "VSPQ",
+				"default": 1,
+				"vin": "1FTFW1EV6AKA75407",
+				"nickname" : "F-150",
+				"year" : "2010",
+				"make" : "Ford",
+				"model" : "F-150 Supercrew",
+				"icon" : "https://s3.amazonaws.com/k-mycloud/a169x672/7BD0B300-7DDF-11E2-AB3A-B9D7E71C24E1.img?q=97013",
+				"notes" : "",
+				"mileage": 159774,
+				"lastWaypoint": {
+					"timestamp": "20140116T152440+0000",
+					"latitude": 37.473896,
+					"longitude": -122.23114
+				},
+				"timestamp": "20140116T151952+0000",
+				"running": false,
+				"fuelRate": 1.2,
+				"fuelLevel" : 37,
+				"coolantTemperature": 163,
+				"batteryVoltage": 13.2,
+				"odometer": 65345,
+				"header": 272,
+				"speed": 0
+			},
+			{
+				"id": "V4GM",
+				"vin": "4TAWNSFDDSKLFJ",
+				"nickname" : "Tacoma",
+				"year" : "2000",
+				"make" : "Toyota",
+				"model" : "Tacoma",
+				"icon" : "https://s3.amazonaws.com/k-mycloud/a169x672/B87948E0-2306-11E3-953D-B39BDC00B96D.img?q=49420",
+				"notes" : "I love this truck!",
+				"mileage": 273000,
+				"lastWaypoint": {
+					"timestamp": "20140116T152440+0000",
+					"latitude": 40.42970,
+					"longitude": -111.89830
+				},
+				"timestamp": "20140116T151952+0000",
+				"running": true,
+				"fuelRate": 1.2,
+				"fuelLevel" : 89,
+				"coolantTemperature": 163,
+				"batteryVoltage": 11.1,
+				"odometer": 65345,
+				"header": 272,
+				"speed": 78
+			},
+			{
+				"id": "VN0E",
+				"vin": "Y257JX9AXQDXI529",
+				"nickname": "Fiesta",
+				"year": "1998",
+				"make": "Ford",
+				"model": "Fiesta",
+				"icon": "style/images/ford_fiesta.jpg",
+				"notes": "Labore consectetur et proident veniam mollit. In minim reprehenderit proident et. Elit fugiat cillum sit cillum quis. Elit ad ullamco velit laborum amet exercitation ea. Nisi ea nostrud ullamco ipsum magna consequat id qui",
+				"mileage": 84519,
+				"lastWaypoint": {
+					"timestamp": "19925606T185601T+07:00",
+					"latitude": 40.74937,
+					"longitude": -111.82633
+				},
+				"timestamp": "19910831T060849T+07:00",
+				"running": true,
+				"fuelRate": 2.63,
+				"fuelLevel": 22,
+				"coolantTemperature": 95,
+				"batteryVoltage": 12.4,
+				"odometer": 19725,
+				"heading": 270,
+				"speed": 12
+			}]
+		},
+		
 		"trips": [{
 			"id": 2689,
 			"distance": 2258.4,
