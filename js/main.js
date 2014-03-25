7b9e20c7d749293da1636595e7a32f31c0855f64
Alex Olson <alexkellenolson@gmail.com>
2014-03-25 15:54:37 -0600

    make dummy data more compelling for demoes.

--- js/main.js
+++ js/main.js
@@ -43,10 +43,11 @@ require(["fuse", "cloudos", "jquery", "routers/app.router", "routers/fleet.route
                 "icon" : "https://s3.amazonaws.com/k-mycloud/a169x672/7BD0B300-7DDF-11E2-AB3A-B9D7E71C24E1.img?q=97013",
                 "notes" : "",
                 "mileage": 159774,
+                "address": "George S. Eccles Dinosaur Park, 1544 Park Blvd, Ogden, UT 84401",
                 "lastWaypoint": {
                     "timestamp": "20140116T152440+0000",
-                    "latitude": 37.473896,
-                    "longitude": -122.23114
+                    "latitude": 41.238178,
+                    "longitude": -111.937246
                 },
                 "timestamp": "20140116T151952+0000",
                 "running": false,
@@ -91,20 +92,21 @@ require(["fuse", "cloudos", "jquery", "routers/app.router", "routers/fleet.route
                 "icon" : "https://s3.amazonaws.com/k-mycloud/a169x672/B87948E0-2306-11E3-953D-B39BDC00B96D.img?q=49420",
                 "notes" : "I love this truck!",
                 "mileage": 273000,
+                "address": "Kynetx, 3098 Executive Pkwy, Lehi, UT 84043",
                 "lastWaypoint": {
                     "timestamp": "20140116T152440+0000",
                     "latitude": 40.42970,
                     "longitude": -111.89830
                 },
                 "timestamp": "20140116T151952+0000",
-                "running": true,
+                "running": false,
                 "fuelRate": 1.2,
                 "fuelLevel" : 89,
                 "coolantTemperature": 163,
                 "batteryVoltage": 11.1,
                 "odometer": 65345,
                 "header": 272,
-                "speed": 78,
+                "speed": 0,
                 "aggregates": {
                     "month": {
                         "trip": {
@@ -139,6 +141,7 @@ require(["fuse", "cloudos", "jquery", "routers/app.router", "routers/fleet.route
                 "icon": "style/images/ford_fiesta.jpg",
                 "notes": "Labore consectetur et proident veniam mollit. In minim reprehenderit proident et. Elit fugiat cillum sit cillum quis. Elit ad ullamco velit laborum amet exercitation ea. Nisi ea nostrud ullamco ipsum magna consequat id qui",
                 "mileage": 84519,
+                "address": "E 9th S, Salt Lake City, UT, 84104",
                 "lastWaypoint": {
                     "timestamp": "19925606T185601T+07:00",
                     "latitude": 40.74937,
@@ -147,12 +150,12 @@ require(["fuse", "cloudos", "jquery", "routers/app.router", "routers/fleet.route
                 "timestamp": "19910831T060849T+07:00",
                 "running": true,
                 "fuelRate": 2.63,
-                "fuelLevel": 22,
+                "fuelLevel": 5,
                 "coolantTemperature": 95,
                 "batteryVoltage": 12.4,
                 "odometer": 19725,
                 "heading": 270,
-                "speed": 12,
+                "speed": 27,
                 "aggregates": {
                     "month": {
                         "trip": {
