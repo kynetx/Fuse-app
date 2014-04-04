7e18c78acf5bc38a666708c1da716a466e3c49d3
Alex Olson <alexkellenolson@gmail.com>
2014-04-03 21:15:57 -0600

    code for fuel view.

---
+++ js/views/fuel.view.js
@@ -0,0 +1,23 @@
+define([ "backbone", "fuse", "jquery", "underscore", "text!templates/fueltmpl.html" ], function( Backbone, Fuse, $, _, fuelTmpl ) {
+    return Fuse.View.extend({
+        id: "fuel",
+        tagName: "div",
+        role: "page",
+        transition: "slide",
+        template: _.template( fuelTmpl ),
+
+        events: {
+            // trigger fillup entry here.
+        },
+
+        initialize: function() {
+            Fuse.View.prototype.initialize.apply( this, arguments );
+            this.header = this.model.get( "nickname" ) + " " + "Fuel";
+        },
+
+        render: function() {
+            // more to come...
+        },
+
+    });
+});
