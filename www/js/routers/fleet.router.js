823c3f6d1507deac3e3b05ce6f9260d88ec9f5fc
Alex Olson <alexkellenolson@gmail.com>
2014-04-17 21:36:55 -0600

    Maintenance Alert stuff and some other goodness.     Formatting in fleet.router     New maintenance alert templates     New maintenance alerts views

--- www/js/routers/fleet.router.js
+++ www/js/routers/fleet.router.js
@@ -1,16 +1,22 @@
 define([ "fuse", "jquery", "underscore" ], function( Fuse, $, _ ) {
     return Fuse.Router.extend({
         routes: {
-            "fleet": "showFleet",
-            "fleet/:id": "showVehicle",
-            "findcar": "showFindCar",
-            "findcar/:id": "showFindCar",
-            "trips": "showTripAggregate",
-            "trips/:id": "showTrips",
-            "fuel": "showFuelAggregate",
-            "fuel/:id": "showFuel",
-            "maintenance": "showMaintenanceSplash",
-            "maintenance/:id": "showMaintenance"
+            "fleet"                     : "showFleet",
+            "fleet/:id"                 : "showVehicle",
+            "findcar"                   : "showFindCar",
+            "findcar/:id"               : "showFindCar",
+            "trips"                     : "showTripAggregate",
+            "trips/:id"                 : "showTrips",
+            "fuel"                      : "showFuelAggregate",
+            "fuel/:id"                  : "showFuel",
+            "maintenance"               : "showMaintenanceSplash",
+            "maintenance/:id"           : "showMaintenance",
+            "maintenance/alerts"        : "showMaintenanceAlerts",
+            "maintenance/:id/alerts"    : "showMaintenanceAlertsForVehicle",
+            "maintenance/reminders"     : "showMaintenanceReminders",
+            "maintenance/:id/reminders" : "showMaintenanceRemindersForeVehicle",
+            "maintenance/history"       : "showMaintenanceHistory",
+            "maintenance/:id/history"   : "showMaintenanceHistoryForVehicle"
         },
 
         showFleet: function() {
