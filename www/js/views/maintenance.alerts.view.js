9d64cfdae926c3a7f17320ef96d89beae8cf784b
Alex Olson <alexkellenolson@gmail.com>
2014-04-18 22:26:42 -0600

    determine for whom we are rendreing. #126.

--- www/js/views/maintenance.alerts.view.js
+++ www/js/views/maintenance.alerts.view.js
@@ -3,7 +3,7 @@ define([ "fuse", "jquery", "underscore", "text!templates/maintenancealertstmpl.h
         id: "maintenance-alerts",
         tagName: "div",
         role: "page",
-        header: "Maintenance Alerts",
+        header: "Fleet Alerts",
         transition: "slide",
         template: _.template( maintenanceAlertsTmpl ),
         
@@ -12,10 +12,18 @@ define([ "fuse", "jquery", "underscore", "text!templates/maintenancealertstmpl.h
 
         initialize: function() {
             Fuse.View.prototype.initialize.apply( this, arguments );
+            if ( this.model ) {
+                this.header = this.model.get( "nickname" ) + " Alerts";
+            }
         },
 
         render: function() {
-            this.content = this.template();
+            // Are we rendering alerts for the whole fleet or just one vehicle?
+            if ( this.model ) {
+                // Just one vehicle.
+            } else {
+                // The whole fleet.
+            }
             Fuse.View.prototype.render.call( this );
         }
 
