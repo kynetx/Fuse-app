4afa0e9f7495af7753283eba4baeac783932a0a9
Alex Olson <alexkellenolson@gmail.com>
2014-04-18 22:50:15 -0600

    alerts view is done. #126.

--- platforms/ios/www/js/views/maintenance.alerts.view.js
+++ platforms/ios/www/js/views/maintenance.alerts.view.js
@@ -15,16 +15,32 @@ define([ "fuse", "jquery", "underscore", "text!templates/maintenancealertstmpl.h
             if ( this.model ) {
                 this.header = this.model.get( "nickname" ) + " Alerts";
             }
+            this.alerts = [];
         },
 
         render: function() {
             // Are we rendering alerts for the whole fleet or just one vehicle?
             if ( this.model ) {
                 // Just one vehicle.
+                this.alerts.append({
+                    vehicle: this.model.get( "nickname" ),
+                    alerts: this.model.get( "alerts" )
+                });
             } else {
                 // The whole fleet.
+                this.controller.fleet.each(function( vehicle ) {
+                    this.collectVehicleAlerts( vehicle );
+                }, this);
             }
+            this.content = this.template({ alerts: this.alerts });
             Fuse.View.prototype.render.call( this );
+        },
+
+        collectVehicleAlerts: function( vehicle ) {
+            this.alerts.append({
+                vehicle: vehicle.get( "nickname" ),
+                alerts: vehicle.get( "alerts" )
+            });
         }
 
     });
