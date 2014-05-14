3968741e94d82e9c3c6ef8eb469edfefc45bd99e
Alex Olson <alexkellenolson@gmail.com>
2014-05-13 21:43:15 -0600

    add notifaction plugin.

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -41,13 +41,29 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.geolocation": "0.3.6",
-    "org.apache.cordova.console": "0.2.7"
+    "org.apache.cordova.console": "0.2.7",
+    "org.apache.cordova.vibration": "0.3.9-dev",
+    "org.apache.cordova.dialogs": "0.2.8-dev"
 }
 // BOTTOM OF METADATA
 });
