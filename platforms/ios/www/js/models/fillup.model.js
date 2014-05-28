2a84281ce79d72faf6f101044942fb472b78b2e3
Alex Olson <alexkellenolson@gmail.com>
2014-05-28 16:18:38 -0600

    add a timestamp to fillup defaults. Defaults to current date & time. IE what new Date() returns given no args.

--- platforms/ios/www/js/models/fillup.model.js
+++ platforms/ios/www/js/models/fillup.model.js
@@ -1,11 +1,12 @@
 define([ "fuse" ], function( Fuse ) {
     return Fuse.Model.extend({
         defaults: {
-            "numGallons": 0,
-            "priceGallon": 0.00,
-            "cost": 0,
-            "odometer": 000000,
-            "gasStation": "Chevron Salem (390 N SR 198, Salem)"
+            "numGallons"    : 0,
+            "priceGallon"   : 0.00,
+            "cost"          : 0,
+            "odometer"      : 000000,
+            "timestamp"     : new Date(),
+            "gasStation"    : "Chevron Salem (390 N SR 198, Salem)"
         }
     });
 });
