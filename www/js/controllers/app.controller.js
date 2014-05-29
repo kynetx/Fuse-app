24e193b0e3c65072ca45cf4071ec3a7bd2c2c53e
Alex Olson <alexkellenolson@gmail.com>
2014-05-29 09:41:48 -0600

    add route for settings.

--- www/js/controllers/app.controller.js
+++ www/js/controllers/app.controller.js
@@ -2,6 +2,10 @@ define(["fuse", "jquery", "underscore"], function(Fuse, $, _) {
     return Fuse.Controller.extend({
         showVehicleListFromHome: function() {
             Fuse.show("fleet");
+        },
+        
+        showSettingsPane: function() {
+            Fuse.show( "settings" );
         }
     });
 });
