c701d398a179ad74f04712180059ef2b4c32e95e
Alex Olson <alexkellenolson@gmail.com>
2014-05-29 11:50:21 -0600

    render the settings view.

--- www/js/controllers/app.controller.js
+++ www/js/controllers/app.controller.js
@@ -1,11 +1,18 @@
 define(["fuse", "jquery", "underscore"], function(Fuse, $, _) {
     return Fuse.Controller.extend({
+
+        init: function() {
+            this.views = {};
+        },
+
         showVehicleListFromHome: function() {
             Fuse.show("fleet");
         },
-        
+
         showSettingsPane: function() {
-            Fuse.show( "settings" );
+            this.views[ "Settings" ] = new SettingsView();
+
+            this.views.Settings.render();
         }
     });
 });
