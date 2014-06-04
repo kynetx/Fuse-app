8eec7eda93801fbf9ffcf954e8e29d4a2cee3ee8
Alex Olson <alexkellenolson@gmail.com>
2014-06-04 16:18:28 -0600

    remove redunant alerts and finish profile skeleton.:trollface:

--- platforms/ios/www/js/views/settings.profile.view.js
+++ platforms/ios/www/js/views/settings.profile.view.js
@@ -14,8 +14,6 @@ define([ "fuse", "jquery", "underscore", "text!templates/settingsprofiletmpl.htm
         render: function() {
             this.content = this.template();
             Fuse.View.prototype.render.call( this );
-
-            alert( "Feature not complete." );
         }
     });
 });
