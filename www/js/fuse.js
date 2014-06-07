a7a797c8b3f18d8b89f101b84a0ae64d25b6b81c
Alex Olson <alexkellenolson@gmail.com>
2014-06-06 23:35:09 -0600

    building 7.8

--- www/js/fuse.js
+++ www/js/fuse.js
@@ -1,7 +1,7 @@
 define(["backbone", "jquery", "underscore", "vendor/google.maps", "text!templates/headertmpl.html", "text!templates/contenttmpl.html", "text!templates/footertmpl.html", "text!templates/menutmpl.html", "text!templates/maptmpl.html"], function(Backbone, $, _, Maps, headerTmpl, contentTmpl, footerTmpl, menuTmpl, mapTmpl) {
     var Fuse = {
         
-        VERSION: "0.0.7.7",
+        VERSION: "0.0.7.8",
 
         history: {
             items: [],
