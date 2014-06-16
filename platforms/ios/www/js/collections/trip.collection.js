114f2110ca56f57fbdcb046c2cb1714e28ced925
Alex Olson <alexkellenolson@gmail.com>
2014-06-16 11:12:19 -0600

    maybe we have to stub an eci?

--- platforms/ios/www/js/collections/trip.collection.js
+++ platforms/ios/www/js/collections/trip.collection.js
@@ -2,6 +2,8 @@ define([ "fuse", "models/trip.model" ], function( Fuse, Trip ) {
     return Fuse.Collection.extend({
         model: Trip,
 
+        // use the api here.
+
         url: Fuse.BASE_API_URI + "trips?_eci=" + localStorage.getItem( "com.kynetx.fuse.ECI" ),
 
         comparator: function( a ) {
