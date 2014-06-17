12c7163946d606a17b3f9c074d720dbcc19344e4
Alex Olson <alexkellenolson@gmail.com>
2014-06-17 03:04:21 -0600

    log the model.

--- platforms/ios/www/js/collections/trip.collection.js
+++ platforms/ios/www/js/collections/trip.collection.js
@@ -2,14 +2,13 @@ define([ "fuse", "models/trip.model", "vendor/fuse.api" ], function( Fuse, Trip,
     return Fuse.Collection.extend({
         model: Trip,
 
-        url: Fuse.BASE_API_URI + "trips?_eci=" + localStorage.getItem( "com.kynetx.fuse.ECI" ),
-
         comparator: function( a ) {
             return FTH.formatDate( a.get( "endTime" ) ) * -1;
         },
-        
+
         sync: function( method, model, options ) {
             Fuse.log( "sync!!!!" );
+            Fuse.log( model );
             options.success();
         }
     });
