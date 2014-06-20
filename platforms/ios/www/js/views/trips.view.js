79903ebe8b95293f2fb93f0417cb306c843f106b
Alex Olson <alexkellenolson@gmail.com>
2014-06-20 02:59:14 -0600

    explicitly cast strings from API to floats so we can calculate aggregates.

--- platforms/ios/www/js/views/trips.view.js
+++ platforms/ios/www/js/views/trips.view.js
@@ -69,8 +69,8 @@ define([ "backbone", "fuse", "jquery", "underscore", "views/trip.view", "views/f
             this.tripViewData[ tripIdx ][ "elements" ].unshift( view.render().el );
             
             this.tripViewData[ tripIdx ][ "aggregates" ][ "duration" ]  += duration;
-            this.tripViewData[ tripIdx ][ "aggregates" ][ "distance" ]  += trip.get( "mileage" );
-            this.tripViewData[ tripIdx ][ "aggregates" ][ "cost" ]      += trip.get( "cost" );
+            this.tripViewData[ tripIdx ][ "aggregates" ][ "distance" ]  += parseFloat( trip.get( "mileage" ) );
+            this.tripViewData[ tripIdx ][ "aggregates" ][ "cost" ]      += parseFloat( trip.get( "cost" ) );
         },
 
         /**
