24943f2f844eb9fcfc3fcca1debe7276fdde8024
Alex Olson <alexkellenolson@gmail.com>
2014-06-23 17:37:03 -0600

    backbone boilerplate x1000000

---
+++ platforms/ios/www/js/views/login.view.js
@@ -0,0 +1,23 @@
+define([ "fuse", "jquery", "underscore", "text!templates/logintmpl.html" ], function( Fuse, $, _, loginTmpl ) {
+    return Fuse.View.extend({
+        id: "login",
+        tagName: "div",
+        role: "page",
+        header: "Login",
+        transition: "slide",
+        template: _.template( loginTmpl ),
+
+        events: {
+            // Collect login info and submit it via CloudOS...
+        },
+
+        initialize: function() {
+            Fuse.View.prototype.initialize.apply( this, arguments );
+        },
+
+        render: function() {
+            this.content = this.template();
+            Fuse.View.prototype.render.call( this );
+        }
+    });
+});
