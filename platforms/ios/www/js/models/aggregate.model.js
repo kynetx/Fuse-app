31393656418676ebeb845f983916e9fd457a166f
Alex Olson <alexkellenolson@gmail.com>
2014-08-05 19:29:22 -0600

    sync with summary beginnings.

--- platforms/ios/www/js/models/aggregate.model.js
+++ platforms/ios/www/js/models/aggregate.model.js
@@ -4,6 +4,43 @@ define([ "fuse", "jquery", "underscore" ], function( Fuse, $, _ ) {
             "distance": 0,
             "cost": 0,
             "time": 0
+        },
+
+        sync: function(method, model, options) {
+            switch(method) {
+                case 'THIS_DOES_NOT_EXIST':
+                    Fuse.loading('show', 'adding fillup...');
+                    API.oneOfTheSummaryMethods(
+
+                        Fuse.currentFuelContext,
+
+                        { 
+                            volume: model.get('numGallons'), 
+                            unitPrice: model.get('priceGallon'), 
+                            odometer: model.get('odometer'),
+                            location: model.get('gasStation'),
+                            when: model.get('timestamp')
+                        }, 
+
+                        function(res) {
+                            Fuse.loading('hide');
+                            if (typeof res.skyCloudError === 'undefined') {
+                                alert('Fillup succesfully added.');
+                                if (typeof options.success === 'function') {
+                                    options.success();
+                                }
+                            } else {
+                                if (typeof options.error === 'function') {
+                                    options.error();
+                                }
+                            }
+                    });
+
+                    break;
+                default:
+                    options.error('API method not yet implemented');
+                    break;
+            }
         }
     });
 });
