6f6cc65a455d462205e99ba76cc7809493cf9686
Alex Olson <alexkellenolson@gmail.com>
2014-08-06 14:05:23 -0600

    new aggregate collection silly git!

--- www/js/models/aggregate.model.js
+++ www/js/models/aggregate.model.js
@@ -4,41 +4,6 @@ define([ "fuse", "jquery", "underscore" ], function( Fuse, $, _ ) {
             "distance": 0,
             "cost": 0,
             "time": 0
-        },
-
-        sync: function(method, model, options) {
-            switch(method) {
-                case 'read':
-                    var now = new Date();
-                    Fuse.loading('show', 'fetching' + ' ' + 'summaries');
-                    API[options.type + 'Summaries'](
-
-                        now.getFullYear(),
-
-                        '0' + (now.getMonth() + 1),
-
-                        function(res) {
-                            Fuse.loading('hide');
-                            if (typeof res.skyCloudError === 'undefined') {
-                                if (typeof options.success === 'function') {
-                                    options.success();
-                                }
-                            } else {
-                                if (typeof options.error === 'function') {
-                                    options.error();
-                                }
-                            }
-                        },
-
-                        {
-                            force: true
-                        }
-                    );
-                    break;
-                default:
-                    options.error('API method not yet implemented');
-                    break;
-            }
         }
     });
 });
