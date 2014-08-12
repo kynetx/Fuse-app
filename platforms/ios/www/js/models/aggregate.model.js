da12ed4154d603ddbc8fdd0f30a2087784bc260e
Alex Olson <alexkellenolson@gmail.com>
2014-08-12 14:39:28 -0600

    dont define defaults

--- platforms/ios/www/js/models/aggregate.model.js
+++ platforms/ios/www/js/models/aggregate.model.js
@@ -1,9 +1,9 @@
 define([ "fuse", "jquery", "underscore" ], function( Fuse, $, _ ) {
     return Fuse.Model.extend({
-        defaults: {
-            "distance": 0,
-            "cost": 0,
-            "time": 0
-        }
+        // defaults: {
+        //    "distance": 0,
+        //    "cost": 0,
+        //    "time": 0
+        // }
     });
 });
