8239733785e84e2edb9ef585753216bbfc1ba0e8
Alex Olson <alexkellenolson@gmail.com>
2014-08-12 16:08:19 -0600

    ok now sum

--- platforms/ios/www/js/models/aggregate.model.js
+++ platforms/ios/www/js/models/aggregate.model.js
@@ -2,8 +2,7 @@ define([ "fuse", "jquery", "underscore" ], function( Fuse, $, _ ) {
     return Fuse.Model.extend({
         defaults: {
             "distance": 0,
-            "cost": 0,
-            "time": 0
+            "cost": 0
         }
     });
 });
