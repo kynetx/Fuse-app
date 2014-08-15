014405dfb941736cfde1f02a3baf19996149582c
Alex Olson <alexkellenolson@gmail.com>
2014-08-15 15:03:37 -0600

    undefined undefined undefined. You know what? If I ever own a horse its name will be "undefined" :horse_racing:

--- www/js/models/trip.model.js
+++ www/js/models/trip.model.js
@@ -100,11 +100,11 @@ define([ "fuse" ], function( Fuse ) {
             switch(method) {
                 case 'read':
                     Fuse.loading('show', 'fetching trip...');
-                    API.trips(Fuse.currentTripContext, model.get('id'), null, null, function(trip) {
+                    API.trips(Fuse.currentTripContext, model.get('id'), null, null, function(res) {
                         Fuse.loading('hide');
                         if (typeof res.skyCloudError === 'undefined') {
                             if (typeof options.success === 'function') {
-                                options.success(trip);
+                                options.success(res);
                             }
                         } else {
                             if (typeof options.error === 'function') {
