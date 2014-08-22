2d612d7a38c85f43a76c8947041b394953a68559
Alex Olson <alexkellenolson@gmail.com>
2014-08-21 18:07:27 -0600

    try

--- platforms/ios/www/js/views/fuel.view.js
+++ platforms/ios/www/js/views/fuel.view.js
@@ -45,11 +45,10 @@ define([ "backbone", "fuse", "jquery", "underscore", "text!templates/fueltmpl.ht
 
             if (Fuse.fuelChart) {
                 for (var i = 0; i < this.costs.length; ++i) {
-                    Fuse.fuelChart.addData(this.costs[i], this.dates[i]);
-                    Fuse.fuelChart.removeData();
+                    Fuse.fuelChart.addData([this.costs[i]], this.dates[i]);
                 }
             } else {
-                Fuse.fuelChart = new Chart( this.chartCanvas ).Line( this.chartData, {animation: false});
+                Fuse.fuelChart = new Chart( this.chartCanvas ).Line( this.chartData );
             }
 
             this.controller.currentFillups.on( "change reset add remove", this.renderChart, this );
