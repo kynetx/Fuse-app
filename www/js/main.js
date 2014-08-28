78dd8c2e6770a2b43cbbeb961ee142b1babc77c8
Alex Olson <alexkellenolson@gmail.com>
2014-08-28 14:46:02 -0600

    ok

--- www/js/main.js
+++ www/js/main.js
@@ -841,6 +841,7 @@
     // start the app.
     document.addEventListener( "deviceready", function() {
         API.init(function() {
+            console.log('API init was called');
             Fuse.init();
         })
     });
