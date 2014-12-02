5a9dcff2e3a4b3993794ac004e738854b6c6cc7d
Phil Windley <phil@windley.org>
2014-12-02 11:13:14 -0700

    Tweaks to maps.
    - Adds check to adding map overlay to ensure position isn't null
    - Enhanced icon labels to use vehicle label, profileName, and name in that order instead of "nickname" which appears to always be "none"

--- www/js/views/findcar.view.js
+++ www/js/views/findcar.view.js
@@ -17,17 +17,33 @@ define([ "fuse", "jquery", "underscore" ], function( Fuse, $, _ ) {
 
             this.collection.each(function(vehicle, idx) {
                 var icon = "style/images/car_map_icon_"+ idx % 3 +".png";
+		console.log("Vehicle in collection: ", vehicle);
+		var label = "";
+		if(this.notEmpty(vehicle.get("label"))) {
+		    label = vehicle.get("label");
+		} else if (this.notEmpty(vehicle.get("profileName"))) {
+		    label = vehicle.get("profileName");
+		} if (this.notEmpty(vehicle.get("name"))) {
+		    label = vehicle.get("name");
+		} 
                 this.map.overlays.push({
                     icon: icon,
                     type: Fuse.map.OverlayTypeId.MARKER,
                     position: vehicle.get("lastWaypoint"),
-                    title: vehicle.get("nickname"),
+                    title: label,
                     animation: "drop",
                     route: "click"
                 });
             }, this);
         },
-        
+
+	notEmpty: function(x) {
+	    return typeof x !== "undefined" &&
+		x !== null &&
+		x !== "none" &&
+		x !== "";
+	},
+
         render: function() {
             Fuse.View.prototype.render.apply( this, arguments );
         }
